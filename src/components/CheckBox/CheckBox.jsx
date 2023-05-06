53f979d21129450bc6030db60c056ca74fd020e2
zverkovnikita09 <zverkovnikita09@gmail.com>
2023-05-07 02:26:48 +0300

    form update

---
+++ src/components/CheckBox/CheckBox.jsx
@@ -0,0 +1,19 @@
+import './CheckBox.css'
+
+const CheckBox = ({id, children, checked, setChecked, ...props}) => {
+  return <div className='checkbox'>
+    <label className='checkbox__label' htmlFor={id}>
+      <input type='checkbox' 
+      className='hidden checkbox__input' 
+      checked={checked} 
+      onChange={setChecked} 
+      id={id}
+      {...props}
+      />
+      <span className='checkbox__box' />
+      {children}
+    </label>
+  </div>
+}
+
+export default CheckBox;
--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,53 +1,56 @@
 import './Filters.css'
 import background from '../../assets/img/background_filters.png'
+import { useState } from 'react'
+import CheckBox from '../CheckBox/CheckBox';
+import { createSearchParams, useNavigate } from 'react-router-dom';
 
 const Filters = () => {
+  const navigate = useNavigate();
+  const [delivery, setDelivery] = useState(false);
+  const [eatPlace, setEatPlace] = useState(false);
+  const [districts, setDistricts] = useState({
+    'Коминтерновский': false,
+    'Центральный': false,
+    'Ленинский': false,
+    'Советский': false,
+    'Железнодорожный': false,
+    'Левобережный': false
+  })
+
+  const updateDistricts = (item) => {
+    const districtsUpdated = { ...districts, [item]: !districts[item] };
+    setDistricts(districtsUpdated);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let params = {}; 
+    navigate({
+      pathname: '/search',
+      search: `?${createSearchParams(params)}`,
+    });
+  }
+
   return <div className='filters'>
     <img src={background} alt="Фон" className='filters__background' width='100%' height='100%' />
     <div className='filters__content'>
-      <form className='filters__form'>
+      <form className='filters__form' onSubmit={e => handleSubmit(e)}>
         <div className='form-group'>
-          <h2 className='form-title'>Цена</h2>
-          <h2 className='form-title'>Ассортимент</h2>
-          <h2 className='form-title'>Качество обслуживания</h2>
-          <label className='filters__label' htmlFor="">
-            <input type='checkbox' className='hidden' />
-            Наличие доставки
-          </label>
-          <label className='filters__label' htmlFor="">
-            <input type='checkbox' className='hidden' />
-            Наличие места для приёма пищи
-          </label>
+          <h2 className='filters__label'>Цена</h2>
+          <h2 className='filters__label'>Ассортимент</h2>
+          <h2 className='filters__label'>Качество обслуживания</h2>
+          <CheckBox id='delivery' checked={delivery} setChecked={e => setDelivery(e.target.checked)}>Наличие доставки</CheckBox>
+          <CheckBox id='eatPlace' checked={eatPlace} setChecked={e => setEatPlace(e.target.checked)}>Наличие места для приема пищи</CheckBox>
         </div>
         <div className='form-group'>
           <h2 className='form-title'>Районы</h2>
           <div className='filters__districts'>
-            <label className='filters__label' htmlFor="">
-              <input type='checkbox' className='hidden' />
-              Коминтерновский
-            </label>
-            <label className='filters__label' htmlFor="">
-              <input type='checkbox' className='hidden' />
-              Центральный
-            </label>
-            <label className='filters__label' htmlFor="">
-              <input type='checkbox' className='hidden' />
-              Ленинский
-            </label>
-            <label className='filters__label' htmlFor="">
-              <input type='checkbox' className='hidden' />
-              Советский
-            </label>
-            <label className='filters__label' htmlFor="">
-              <input type='checkbox' className='hidden' />
-              Железнодорожный
-            </label>
-            <label className='filters__label' htmlFor="">
-              <input type='checkbox' className='hidden' />
-              Левобережный
-            </label>
+            {Object.keys(districts).map((item, index) => {
+              return <CheckBox id={item} value={item} checked={districts[item]} setChecked={e => updateDistricts(e.target.value)}>{item}</CheckBox>
+            })}
           </div>
         </div>
+        <button>Искать</button>
       </form>
     </div>
   </div>
